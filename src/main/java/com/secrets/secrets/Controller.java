da98952f3b5d52ebf551d75066c325c8044722bc
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-08-26 14:30:28 +0200

    Fungerar lokalt

--- src/main/java/com/secrets/secrets/Controller.java
+++ src/main/java/com/secrets/secrets/Controller.java
@@ -1,12 +1,20 @@
 package com.secrets.secrets;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @Value("${nonsecret.key}")
+    String nonsecretKey;
+
+    @Value("${secret.key}")
+    String secretKey;
+
     @GetMapping
     public String getTest() {
-        return "Hej";
+        return "Icke hemlig nyckel: " + nonsecretKey + " HEMLIG NYCKEL: " + secretKey;
     }
 }
